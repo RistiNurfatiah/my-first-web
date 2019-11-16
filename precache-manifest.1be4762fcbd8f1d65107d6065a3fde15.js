3fc874647005a03cbde541c86c3da55d515f1579
Risti.nurfatiah 49 <fatiah2509@yahoo.com>
2019-11-16 14:07:53 +0700

    init

---
+++ precache-manifest.1be4762fcbd8f1d65107d6065a3fde15.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75cccf9790bc62fbbbde7671266c52f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a068af0ff7e310392ec",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d3613876dd6d41005e14",
+    "url": "/static/js/2.45d19aa5.chunk.js"
+  },
+  {
+    "revision": "5a068af0ff7e310392ec",
+    "url": "/static/js/main.43cabe5f.chunk.js"
+  },
+  {
+    "revision": "c8a11d60274b684aa865",
+    "url": "/static/js/runtime-main.2e96e0f3.js"
+  },
+  {
+    "revision": "20bbb5b8c31a40b326b54c18295f3d16",
+    "url": "/static/media/bg.20bbb5b8.png"
+  }
+]);
